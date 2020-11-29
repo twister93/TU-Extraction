e8f873edc2698facf7ad76534d4e5c0756e0518d
Alvarez Gallego Santiago (STAG) <st2alvarsa@group.pirelli.com>
2020-11-29 18:34:18 +0100

    first commit

---
+++ TU_Extraction.py
@@ -0,0 +1,15 @@
+# Format code shift+alt+f  sort imports
+import sys
+from datetime import datetime
+from tkinter import *
+
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import create_engine
+
+print(sys.version)
+print(sys.executable)
+
+
+# root = Tk(className=' PCS Indu Status Checker ')
+# entry = Entry(root,bd=3, width=15)
